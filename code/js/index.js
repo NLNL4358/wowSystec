7e2e6c7e289f4bb86fa44b5a98a2ee67ee54b1b3
강진수 <wide4514@ncubeworld.com>
2025-01-02 18:26:03 +0900

    25.01.02 작업

--- code/js/index.js
+++ code/js/index.js
@@ -1,52 +1,69 @@
 $(document).ready(() => {
-/*** Variable */
-    /**
-     * @param urlObject - 페이지 html의 주소를 가지고있는 객체
-     */
-    const urlObject = {
-        HOME : "/index.html",
-        INTRODUCTION_URL : "/page/introduction.html",
-        PRODUCT_SERVICE_URL : "/page/productService.html",
-        CONTACT_URL : "/page/contact.html",
+  /*** Variable */
+  /**
+   * @param urlObject - 페이지 html의 주소를 가지고있는 객체
+   */
+  const urlObject = {
+    HOME: "/index.html",
+    INTRODUCTION_URL: "/page/introduction.html",
+    PRODUCT_SERVICE_URL: "/page/productService.html",
+    CONTACT_URL: "/page/contact.html",
+  };
+  const gnbUrlArray = [
+    "INTRODUCTION_URL",
+    "PRODUCT_SERVICE_URL",
+    "CONTACT_URL",
+  ];
+
+  /*** DOM */
+  const gnb = document.querySelectorAll(".gnb");
+  const logoButton = document.querySelector(".logoButton");
+  const header = document.querySelector(".header");
+  const snbWrap = document.querySelector(".snbWrap");
+
+  /*** Function */
+  const goToPage = (target) => {
+    switch (target) {
+      case "HOME":
+        window.location.href = urlObject.HOME;
+        break;
+      case "CONTACT_URL":
+        window.location.href = urlObject.CONTACT_URL;
+        break;
+      case "INTRODUCTION_URL":
+        window.location.href = urlObject.INTRODUCTION_URL;
+        break;
+      case "PRODUCT_SERVICE_URL":
+        window.location.href = urlObject.PRODUCT_SERVICE_URL;
+        break;
     }
-    const gnbUrlArray = [
-        "INTRODUCTION_URL",
-        "PRODUCT_SERVICE_URL",
-        "CONTACT_URL",
-    ]
+  };
 
-/*** DOM */
-    const gnb = document.querySelectorAll(".gnb");
-    const logoButton = document.querySelector(".logoButton")
-
-/*** Function */
-    const goToPage = (target) => {
-        switch(target){
-            case "HOME" :
-                window.location.href = urlObject.HOME
-                break
-            case "CONTACT_URL" :
-                window.location.href = urlObject.CONTACT_URL
-                break
-            case "INTRODUCTION_URL" :
-                window.location.href = urlObject.INTRODUCTION_URL
-                break
-            case "PRODUCT_SERVICE_URL" :
-                window.location.href = urlObject.PRODUCT_SERVICE_URL
-                break
-
-        }
+  const snbControl = (toggle) => {
+    switch (toggle) {
+      case "on":
+        snbWrap.classList.add("on");
+        break;
+      case "off":
+        snbWrap.classList.remove("on");
+        break;
     }
+  };
 
-/*** Event */
-    gnb.forEach((element, index) => {
-        element.addEventListener("click", () => {
-            goToPage(gnbUrlArray[index])
-        })
+  /*** Event */
+  gnb.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      goToPage(gnbUrlArray[index]);
     });
-    logoButton.addEventListener("click", () => {
-        goToPage("HOME")
-    })
-
-/*** Init */
+  });
+  logoButton.addEventListener("click", () => {
+    goToPage("HOME");
+  });
+  header.addEventListener("mouseover", () => {
+    snbControl("on");
+  });
+  header.addEventListener("mouseout", () => {
+    snbControl("off");
+  });
+  /*** Init */
 });
--- code/js/swiper.js
+++ code/js/swiper.js
@@ -1,3 +1,6 @@
+/*** DOM */
+const swiperNavigation = document.querySelectorAll(".swiperNavigation");
+
 var swiper = new Swiper(".mainSwiper", {
   direction: "vertical", // 수직방향
   grabCursor: false, // 그랩 커서 false
@@ -11,7 +14,59 @@ var swiper = new Swiper(".mainSwiper", {
     invert: false,
   },
 
-  // pagination
+  // 추가 Effect
+  // effect: "creative",
+  // creativeEffect: {
+  //   prev: {
+  //     shadow: false,
+  //     translate: [0, "-100%", -1],
+  //   },
+  //   next: {
+  //     translate: [0, "100%", 0],
+  //   },
+  // },
+
+  on: {
+    slideChange: () => {
+      console.log(this?.swiper?.realIndex);
+      if (this?.swiper?.realIndex !== undefined) {
+        swiperNavigationChanger(this?.swiper?.realIndex);
+
+        switch (this.swiper.realIndex) {
+          case 0:
+            headerClassChanger("white");
+            break;
+          case 1:
+            headerClassChanger("white");
+            break;
+          default:
+            headerClassChanger("default");
+            break;
+        }
+      }
+    },
+  },
+});
+const headerClassChanger = (toggle) => {
+  if (toggle === "white") {
+    document.querySelector(".header").classList.add("white");
+    document.querySelector(".swiperNavigationWrap").classList.add("white");
+  } else {
+    document.querySelector(".header").classList.remove("white");
+    document.querySelector(".swiperNavigationWrap").classList.remove("white");
+  }
+};
+const swiperNavigationChanger = (index) => {
+  swiperNavigation.forEach((item) => {
+    item.classList.remove("on");
+  });
+  swiperNavigation[index].classList.add("on");
+};
+
+var subSwiper = new Swiper(".subSwiper", {
+  spaceBetween: 0,
+  loop: true, // 무한 루프
+
   pagination: {
     el: ".swiper-pagination",
   },
@@ -20,27 +75,5 @@ var swiper = new Swiper(".mainSwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  },
-
-  // 추가 Effect
-  effect: "creative",
-  creativeEffect: {
-    prev: {
-      shadow: false,
-      translate: [0, "-100%", -1],
-    },
-    next: {
-      translate: [0, "100%", 0],
-    },
-  },
-});
-
-var swiper = new Swiper(".subSwiper", {
-  spaceBetween: 0,
-  loop: true, // 무한 루프
-
-  pagination: {
-    el: ".subSwiper-pagination",
-    clickable: true,
   },
 });
